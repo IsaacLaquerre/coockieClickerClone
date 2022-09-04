fdf4e0eecddf434803c81dbc73f9581ca29cc316
IsaacLaquerre <killers3d@gmail.com>
2022-09-04 13:34:50 -0400

    Added shop some upgrades, some more shop items, restart button and more i forgot

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,5 +1,6 @@
 var cookieCount = 0;
 var cpsCount = 0;
+var cookiesPerClick = 1;
 var priceIncrease = 1.15;
 var playSounds = true;
 var animate = true;
@@ -19,7 +20,135 @@ const items = [
     { name: "Grandma", price: 100, basePrice: 100, cps: 1, owned: 0 },
     { name: "Farm", price: 1100, basePrice: 1100, cps: 8, owned: 0 },
     { name: "Mine", price: 12000, basePrice: 12000, cps: 47, owned: 0 },
-    { name: "Factory", price: 130000, basePrice: 130000, cps: 260, owned: 0 }
+    { name: "Factory", price: 130000, basePrice: 130000, cps: 260, owned: 0 },
+    { name: "Bank", price: 1400000, basePrice: 1400000, cps: 1400, owned: 0 },
+    { name: "Temple", price: 20000000, basePrice: 20000000, cps: 7800, owned: 0 },
+    { name: "Wizard Tower", price: 330000000, basePrice: 330000000, cps: 44000, owned: 0 }
+];
+
+const upgrades = [{
+        name: "Reinforced index finger",
+        description: "The mouse and cursors are twice as efficient.",
+        price: 100,
+        basePrice: 100,
+        exec: () => {
+            cookiesPerClick = cookiesPerClick * 2;
+            localStorage.setItem("cpc", cookiesPerClick);
+            var cursor = items.find(shopItem => shopItem.name === "Cursor");
+            cursor.cps = cursor.cps * 2;
+            recalculateCps();
+            checkAvailableItems();
+        },
+        owned: 0
+    },
+    {
+        name: "Carpal tunnel prevention cream",
+        description: "The mouse and cursors are twice as efficient.",
+        price: 500,
+        basePrice: 500,
+        exec: () => {
+            cookiesPerClick = cookiesPerClick * 2;
+            localStorage.setItem("cpc", cookiesPerClick);
+            var cursor = items.find(shopItem => shopItem.name === "Cursor");
+            cursor.cps = cursor.cps * 2;
+            recalculateCps();
+            checkAvailableItems();
+        },
+        owned: 0
+    },
+    {
+        name: "Ambidextrous",
+        description: "The mouse and cursors are twice as efficient.",
+        price: 10000,
+        basePrice: 10000,
+        exec: () => {
+            cookiesPerClick = cookiesPerClick * 2;
+            localStorage.setItem("cpc", cookiesPerClick);
+            var cursor = items.find(shopItem => shopItem.name === "Cursor");
+            cursor.cps = cursor.cps * 2;
+            recalculateCps();
+            checkAvailableItems();
+        },
+        owned: 0
+    },
+    {
+        name: "Forwards from grandma",
+        description: "Grandmas are twice as efficient.",
+        price: 1000,
+        basePrice: 1000,
+        exec: () => {
+            var grandma = items.find(shopItem => shopItem.name === "Grandma");
+            grandma.cps = grandma.cps * 2;
+            recalculateCps();
+            checkAvailableItems();
+        },
+        owned: 0
+    },
+    {
+        name: "Steel-plated rolling pins",
+        description: "Grandmas are twice as efficient.",
+        price: 5000,
+        basePrice: 5000,
+        exec: () => {
+            var grandma = items.find(shopItem => shopItem.name === "Grandma");
+            grandma.cps = grandma.cps * 2;
+            recalculateCps();
+            checkAvailableItems();
+        },
+        owned: 0
+    },
+    {
+        name: "Lubricated dentures",
+        description: "Grandmas are twice as efficient.",
+        price: 50000,
+        basePrice: 50000,
+        exec: () => {
+            var grandma = items.find(shopItem => shopItem.name === "Grandma");
+            grandma.cps = grandma.cps * 2;
+            recalculateCps();
+            checkAvailableItems();
+        },
+        owned: 0
+    },
+    {
+        name: "Prune juice",
+        description: "Grandmas are twice as efficient.",
+        price: 5000000,
+        basePrice: 5000000,
+        exec: () => {
+            var grandma = items.find(shopItem => shopItem.name === "Grandma");
+            grandma.cps = grandma.cps * 2;
+            recalculateCps();
+            checkAvailableItems();
+        },
+        owned: 0
+    },
+    {
+        name: "Double-thick glasses",
+        description: "Grandmas are twice as efficient.",
+        price: 500000000,
+        basePrice: 500000000,
+        exec: () => {
+            var grandma = items.find(shopItem => shopItem.name === "Grandma");
+            grandma.cps = grandma.cps * 2;
+            recalculateCps();
+            checkAvailableItems();
+        },
+        owned: 0
+    },
+    {
+        name: "Aging agents",
+        description: "Grandmas are twice as efficient.",
+        price: 50000000000,
+        basePrice: 50000000000,
+        exec: () => {
+            var grandma = items.find(shopItem => shopItem.name === "Grandma");
+            grandma.cps = grandma.cps * 2;
+            recalculateCps();
+            checkAvailableItems();
+        },
+        owned: 0
+    }
 ];
 
 const animations = {
@@ -130,11 +259,22 @@ function getStoredCps() {
     else localStorage.setItem("cps", 0);
 }
 
+function getStoredCpc() {
+    var cpc = localStorage.getItem("cpc");
+    if (cpc) setCpc(cpc);
+    else localStorage.setItem("cpc", 1);
+}
+
 function getStoredItems() {
     for (i in items) {
         var storage = localStorage.getItem(items[i].name);
         if (storage) setBoughtItem(items[i].name, storage);
         else localStorage.setItem(items[i].name, 0);
+    }
+    for (i in upgrades) {
+        var storage = localStorage.getItem(upgrades[i].name);
+        if (storage) setBoughtUpgrade(upgrades[i].name, storage);
+        else localStorage.setItem(upgrades[i].name, 0);
     }
 }
 
@@ -180,21 +320,49 @@ function setCookies(amount) {
 
 function setCps(amount) {
     var cpsCounter = document.getElementById("cpsCount");
-    cpsCount = amount;
+    cpsCount = parseFloat(amount);
     cpsCounter.innerHTML = Math.floor(amount * 10) / 10;
 }
 
+function setCpc(amount) {
+    //cookiesPerClick = parseInt(amount);
+}
+
 function setBoughtItem(item, amount) {
-    var shopItem = items.find(shItem => shItem.name === item);
+    var shopItem = items.find(shItem => shItem.name === item) || upgrades.find(shItem => shItem.name === item);
     shopItem.owned = amount;
     document.getElementById(item.toLowerCase()).children[2].innerHTML = "Owned: " + amount;
     recalculatePrice(shopItem);
     document.getElementById(item.toLowerCase()).children[1].children[1].innerHTML = " " + shopItem.price;
+    recalculateCps();
+}
+
+function setBoughtUpgrade(upgrade, amount) {
+    var shopItem = upgrades.find(shItem => shItem.name === upgrade);
+    shopItem.owned = amount;
+    if (amount != 0) shopItem.exec();
 }
 
 function setUpShop() {
 
-    var shop = document.getElementById("shop");
+    var itemsSection = document.getElementById("items");
+    var upgradesSection = document.getElementById("upgrades");
+
+    for (i in upgrades) {
+        if (upgrades[i].owned != 0) return;
+        var upgrade = document.createElement("div");
+        upgrade.classList.add("upgrade");
+        upgrade.id = upgrades[i].name.toLowerCase();
+        upgrade.setAttribute("onclick", "buy(this.id)");
+        upgrade.title = upgrades[i].name + "\n" + upgrades[i].description + "\n" + upgrades[i].price + " cookies";
+
+        var upgradeImg = document.createElement("img");
+        upgradeImg.src = "assets/images/shop/upgrades/" + upgrades[i].name + ".png";
+
+        upgrade.appendChild(upgradeImg);
+
+        upgradesSection.appendChild(upgrade);
+    }
 
     for (i in items) {
         var item = document.createElement("div");
@@ -203,7 +371,7 @@ function setUpShop() {
         item.setAttribute("onclick", "buy(this.id);");
 
         var itemImg = document.createElement("img");
-        itemImg.src = "assets/images/shop/" + items[i].name + ".png";
+        itemImg.src = "assets/images/shop/items/" + items[i].name + ".png";
 
         item.appendChild(itemImg);
 
@@ -234,7 +402,7 @@ function setUpShop() {
 
         item.appendChild(itemOwned);
 
-        shop.appendChild(item);
+        itemsSection.appendChild(item);
     }
 }
 
@@ -246,13 +414,25 @@ function checkAvailableItems() {
             item.classList.remove("unavailable");
         } else item.classList.add("unavailable");
     }
+    for (i in upgrades) {
+        var upgrade = document.getElementById(upgrades[i].name.toLowerCase());
+        if (upgrade) {
+            if (upgrades[i].owned != 0) {
+                recalculateCps();
+                return upgrade.remove();
+            }
+            if (cookies >= upgrades[i].price) {
+                upgrade.classList.remove("unavailable");
+            } else upgrade.classList.add("unavailable");
+        }
+    }
 }
 
 function clicked(event) {
     var cookie = document.getElementById("cookie");
     cookie.animate(animations.clicked.frames, animations.clicked.options);
     var cookieCounter = document.getElementById("cookieCount");
-    var cookies = parseInt(cookieCounter.innerHTML) + 1;
+    var cookies = parseInt(cookieCounter.innerHTML) + parseInt(cookiesPerClick);
     cookieCount = cookies;
     cookieCounter.innerHTML = cookies;
     localStorage.setItem("cookies", cookies);
@@ -310,7 +490,7 @@ function throwCookie(coords) {
 function dropCookie() {
     if (!animate) return;
     var cookie = document.createElement("img");
-    var random = Math.floor(Math.random() * (window.innerWidth * 0.3 - 75));
+    var random = Math.floor(Math.random() * (window.innerWidth * 0.3 - 50));
     cookie.classList.add("pixelCookie");
     cookie.src = "assets/images/cookie_pixelated.png";
     cookie.width = "50";
@@ -334,4 +514,26 @@ function dropCookie() {
     setTimeout(() => {
         cookie.remove();
     }, 1250);
+}
+
+function removeLoader() {
+    var loader = document.getElementById("load");
+    var opacity = 1;
+    var fadeOut = setInterval(() => {
+        loader.style.opacity = opacity;
+        if (opacity <= 0) {
+            loader.remove();
+            return clearInterval(fadeOut);
+        }
+        opacity -= 0.025;
+    }, 5);
+}
+
+function restart() {
+    if (confirm("Are you sure you want to restart and lose all your progress?")) {
+        localStorage.clear();
+        recalculateCps();
+        setCookies(0);
+        window.location.reload();
+    }
 }
--- assets/js/shop.js
+++ assets/js/shop.js
@@ -2,18 +2,18 @@ var boughtAudio = new Audio("assets/audio/bought.mp3");
 
 function buy(item) {
     var element = document.getElementById(item);
-    var shopItem = items.find(shopItem => shopItem.name === item.charAt(0).toUpperCase() + item.slice(1));
+    var shopItem = items.find(shopItem => shopItem.name === item.charAt(0).toUpperCase() + item.slice(1)) || upgrades.find(shopItem => shopItem.name === item.charAt(0).toUpperCase() + item.slice(1));
     var price = shopItem.price;
     if (cookieCount >= price) {
         cookieCount -= price;
         shopItem.owned = parseInt(shopItem.owned) + 1;
         localStorage.setItem(shopItem.name, shopItem.owned);
-        setBoughtItem(shopItem.name, shopItem.owned);
+        if (!shopItem.exec) setBoughtItem(shopItem.name, shopItem.owned);
+        else setBoughtUpgrade(shopItem.name, shopItem.owned);
         setCookies(cookieCount);
         localStorage.setItem("cookies", cookieCount);
-        cpsCount = parseFloat(cpsCount) + shopItem.cps;
-        setCps(cpsCount);
-        localStorage.setItem("cps", cpsCount);
+        if (!shopItem.exec) cpsCount = parseFloat(cpsCount) + shopItem.cps;
+        recalculateCps();
         element.animate(animations.bought.frames, animations.bought.options);
         if (playSounds) boughtAudio.play();
     } else {
@@ -24,5 +24,23 @@ function buy(item) {
 
 function recalculatePrice(item) {
     var price = item.basePrice * Math.pow(priceIncrease, Math.max(0, item.owned));
-    items.find(shopItem => shopItem.name === item.name).price = Math.ceil(price);
+    try {
+        items.find(shopItem => shopItem.name === item.name).price = Math.ceil(price);
+        upgrades.find(shopItem => shopItem.name === item.name).price = Math.ceil(price);
+    } catch (e) {}
+}
+
+function recalculateCps() {
+    var cps = localStorage.getItem("cps");
+    var totalCps = 0;
+    for (i in items) {
+        var item = document.getElementById(items[i].name.toLowerCase());
+        item.children[1].children[2].innerHTML = "CPS: " + items[i].cps;
+        totalCps = parseFloat(totalCps) + parseFloat(items[i].owned * items[i].cps);
+    }
+
+    if (totalCps != cps) {
+        setCps(totalCps);
+        localStorage.setItem("cps", totalCps);
+    }
 }
